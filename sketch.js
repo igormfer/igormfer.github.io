fee0fdab652044926fa1038b3e11a7e64c129e39
igormfer <igor.mfer@gmail.com>
2022-04-06 23:39:41 -0300

    v2

--- sketch.js
+++ sketch.js
@@ -5,14 +5,19 @@ let diameterBall = 15;
 let radius = diameterBall / 2;
 
 // Ball Speed Variables
-let speedXBall = 8;
-let speedYBall = 8;
+let speedXBall = 3;
+let speedYBall = 6;
 
 // My rect variables
 let xmyRacket = 5;
-let heightmyRacket = 100;
+let heightRacket = 100;
 let ymyRacket = 150
-let widthmyRacket = 15;
+let widthRacket = 15;
+
+//Openent rect variables
+let xOpponentRect = 480 
+let yOpponentRect = 150
+let moveYOpponentRect;
 
 
 function setup() {
@@ -25,10 +30,12 @@ function draw() {
   showBall();
   speedBall();
   validateEdgeColision();
-  myRacket();
+  showRacket(xmyRacket,ymyRacket);
   movemyRacket();
-  checkColisionmyRacket();
- 
+//  checkColisionmyRacket();
+  myRectCollide();
+  showRacket(xOpponentRect,yOpponentRect);
+  moveOpponentRacket();
 }
 
 
@@ -48,7 +55,6 @@ function speedBall() {
 
 // Function for check the ball hits the edge
 function validateEdgeColision() {
-  
   //* width is the right side limit or X
   //*  xBall + radius > width is the outside of the ball 
   if (xBall + radius > width || xBall - radius < 0){
@@ -61,12 +67,12 @@ function validateEdgeColision() {
   }
 }
 
-// My Rect, i.e, the who I control
-function myRacket(){
-//    rect(xmyRacket,ymyRacket,widthmyRacket,heightmyRacket);
-    rect(xmyRacket,ymyRacket,widthmyRacket,heightmyRacket);
+// Show any Rect if you set x and y parameters 
+function showRacket(x,y){
+    rect(x,y,widthRacket,heightRacket);
 }
 
+// move my rect up and down
 function movemyRacket(){
   if (keyIsDown(UP_ARROW)){
     ymyRacket -= 5
@@ -81,7 +87,31 @@ function movemyRacket(){
 
 //----> Falta completar o resto da aula. A partir do minuto 4 mais ou menos.
 function checkColisionmyRacket(){
-  if (xBall - radius < xmyRacket + widthmyRacket && yBall - radius < ymyRacket + heightmyRacket){
+  if (xBall - radius < xmyRacket + widthRacket && yBall - radius < ymyRacket + heightRacket){
        speedXBall *= -1
   }
 }
+
+// collideRectCircle is a library function.
+// This function returns True or False for
+// colision between circle and Rect
+
+function myRectCollide(){
+  collided = 
+  collideRectCircle(xmyRacket, ymyRacket,widthRacket,heightRacket, xBall, yBall, radius)
+  
+  if(collided){
+    speedXBall *= -1
+  }
+}
+
+function moveOpponentRacket(){
+  moveYOpponentRect = yBall - yOpponentRect - widthRacket / 2 - 70
+  yOpponentRect += moveYOpponentRect
+  
+  }
+
+
+
+
+
